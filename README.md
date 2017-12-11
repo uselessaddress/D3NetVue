dab0cbe43a5082a730b49f11bf7de9e9d5cf49ae
Constantin Wang <850901938@qq.com>
2017-12-11 17:26:31 +0800

    finished

---
+++ README.md
@@ -0,0 +1,10 @@
+## Integrate D3 network with Vue
+### result:
+
+![result](./result.png)
+
+### links
+
+[D3-force Doc](https://github.com/d3/d3-force/blob/master/README.md#forceSimulation)  
+
+[Introduce D3-force](http://mat617.com/%E6%95%B0%E6%8D%AE%E5%8F%AF%E8%A7%86%E5%8C%96/2017/02/18/%E5%8A%9B%E5%AF%BC%E5%90%91%E5%9B%BE%E7%AE%80%E4%BB%8B(Force-Directed-Graph)-in-D3.V4/#5-forcelink)
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <D3Force></D3Force>
+  </div>
+</template>
+
+<script>
+import D3Force from './components/D3_force.vue'
+export default {
+  name: 'app',
+  components: { D3Force }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
